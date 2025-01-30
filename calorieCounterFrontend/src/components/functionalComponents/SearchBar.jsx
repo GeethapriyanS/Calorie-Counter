a542c7a849f10cfc736088d7005b00d60678d515
GeethapriyanS <geethapriyan.s2023cse@sece.ac.in>
2025-01-30 12:54:53 +0530

    vercel deploy

--- calorieCounterFrontend/src/components/functionalComponents/SearchBar.jsx
+++ calorieCounterFrontend/src/components/functionalComponents/SearchBar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import "../../css/SearchBar.css";
+import "../../css/Searchbar.css";
 
 const SearchBar = ({ setMeals }) => {
     const [query, setQuery] = useState("");
